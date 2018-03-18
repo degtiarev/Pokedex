ff90cb8e0afaa18d55f938a0b8fd6dab8fd215a3
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-18 23:43:27 +0100

    Added properties and outlets for pokemon

--- pokedex/Controller/PokemonDetailVC.swift
+++ pokedex/Controller/PokemonDetailVC.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenceLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
     
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        
     }
     
+    
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
     
 }
