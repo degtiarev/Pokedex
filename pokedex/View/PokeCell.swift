022e583b1083c76b45863a57464ff5b6ff1856f9
Aleksei Degtiarev <delexa0@gmail.com>
2018-03-18 04:40:38 +0100

    Released methods for CollectionView

--- pokedex/View/PokeCell.swift
+++ pokedex/View/PokeCell.swift
@@ -9,16 +9,22 @@
 import UIKit
 
 class PokeCell: UICollectionViewCell {
-
+    
     @IBOutlet weak var thumbImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-
+    
     var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
     
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
         nameLabel.text = self.pokemon.Name.capitalized
         thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
     }
-
+    
 }
